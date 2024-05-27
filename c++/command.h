4799a424571aaab78b8bb16b7ae61515a7498a37
bostonsinaga <bostonsinga@gmail.com>
2024-05-27 13:25:24 +0700

    Create stand-alone Parameters and Toggles classes

--- c++/command.h
+++ c++/command.h
@@ -6,34 +6,79 @@
 
 namespace namespace cli_menu {
 
+  class Parameters {
+  private:
+    VEC_STR *names;
+    VEC_INT *types;
+    VEC_BOL *required;
+    VEC_STR *descriptions;
+
+  public:
+    Parameters(
+      VEC_STR names_in,
+      VEC_INT types_in,
+      VEC_BOL required_in,
+      VEC_STR descriptions_in
+    ) {
+      names = &names_in;
+      types = &types_in;
+      required = &required_in;
+      descriptions = &descriptions_in;
+    }
+
+    VEC_STR *getNames() { return names; }
+    VEC_INT *getTypes() { return types; }
+    VEC_BOL *getRequired() { return required; }
+    VEC_STR *getDescriptions() { return descriptions; }
+  };
+
+  class Toggles {
+  private:
+    VEC_STR *names;
+    VEC_STR *descriptions;
+
+  public:
+    Toggles(
+      VEC_STR names_in,
+      VEC_STR descriptions_in
+    ) {
+      names = &names_in;
+      descriptions = &descriptions_in;
+    }
+
+    VEC_STR *getNames() { return names; }
+    VEC_STR *getDescriptions() { return descriptions; }
+  };
+
   class Command {
   private:
     std::string *name;
     std::string *description;
-    VEC_STR *parameters;
-    VEC_STR *toggles;
-    std::function<VEC_STR, VEC_BOL, VEC_INT> *callback;
+    Parameters *parameters;
+    Toggles *toggles;
+    std::function<VEC_STR, VEC_DBL, VEC_BOL> *callback;
 
   public:
     Command(
       std::string name_in,
       std::string description_in,
-      VEC_STR parameters_in,
-      VEC_STR toggles_in,
-      std::function<VEC_STR, VEC_BOL, VEC_INT> callback_in
+      Parameters *parameters_in,
+      Toggles *toggles_in,
+      std::function<VEC_STR, VEC_DBL, VEC_BOL> callback_in
     ) {
       name = &name_in;
       description = &description_in;
-      parameters = &parameters_in;
-      toggles = &toggles_in;
+      parameters = parameters_in;
+      toggles = toggles_in;
       callback = &callback_in;
     }
 
     std::string getName() { return *name; }
-    VEC_STR *getParameters() { return parameters; }
-    VEC_STR *getToggles() { return toggles; }
     std::string getDescription() { return *description; }
     void execute() { callback(); }
+
+    Parameters *getParameters() { return parameters; }
+    Toggles *getToggles() { return toggles; }
   };
 }
 
