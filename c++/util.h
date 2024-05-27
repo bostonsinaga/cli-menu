5ea42127188379f49582e5632ba6a275971ede4e
bostonsinaga <bostonsinga@gmail.com>
2024-05-27 13:30:48 +0700

    Revise types name

--- c++/util.h
+++ c++/util.h
@@ -5,15 +5,19 @@
 
 namespace cli_menu {
   typedef std::vector<std::string> VEC_STR;
-  typedef std::vector<bool> VEC_BOL;
+  typedef std::vector<double> VEC_DOU;
+  typedef std::vector<bool> VEC_BOO;
   typedef std::vector<int> VEC_INT;
+
+  // char, string, integer, float
+  enum {CHA, STR, INT, FLO};
 
   class Util {
   public:
     static void stringsToLowercase(VEC_STR *vecStr);
     static void cutVector(VEC_STR *vecStr, int index);
     static void cleanDuplicateStrings(VEC_STR *vecStr);
-    static void cleanDuplicatesToName(VEC_STR *vecStr, std::sdtring *name);
+    static void cleanDuplicatesToName(VEC_STR *vecStr, std::string name);
   };
 }
 
