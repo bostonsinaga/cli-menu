acb5df1ff1b27cb65d780b023158c962122b64b8
bostonsinaga <bostonsinga@gmail.com>
2024-06-04 09:26:27 +0700

    Directly define template methods

--- c++/util.h
+++ c++/util.h
@@ -16,18 +16,26 @@ namespace cli_menu {
   typedef std::vector<bool> VEC_BOO;
   typedef std::vector<int> VEC_INT;
 
-  namespace Util {
-    template <typename T> bool isIndexOfVector(std::vector<T> *vec, int idx) {
+  class Util {
+  public:
+    static void changeStringCase(std::string &str, bool isUpper);
+    static void changeStringsCase(std::vector<std::string> *vecStr, bool isUpper);
+
+  public:
+    template <typename T>  
+    static bool isIndexOfVector(std::vector<T> *vec, int idx) {
       if (idx < vec->size() && idx >= 0) return true;
       return false;
     }
 
-    template <typename T> T getIndexOfVector(std::vector<T> *vec, int idx, T defaultReturn) {
+    template <typename T>  
+    static T getIndexOfVector(std::vector<T> *vec, int idx, T defaultReturn) {
       if (isIndexOfVector<T>(vec, idx)) return vec->at(idx);
       return defaultReturn;
     }
 
-    template <typename T> T cutVector(std::vector<T> *vec, int idx) {
+    template <typename T>
+    static T cutVector(std::vector<T> *vec, int idx) {
       T wasted = vec->at(idx);
       std::vector<T> tail = std::vector<T>(vec->begin() + idx + 1, vec->end());
       *vec = std::vector<T>(vec->begin(), vec->begin() + idx);
@@ -35,7 +43,8 @@ namespace cli_menu {
       return wasted;
     }
 
-    template <typename T> std::vector<T> cleanDuplicateInsideVector(
+    template <typename T>
+    static std::vector<T> cleanDuplicateInsideVector(
       std::vector<T> *vec,
       // repeated and compared 'T' parameters
       std::function<bool(T,T)> equalRule = [](T rep, T com)->bool { return false; }
@@ -64,7 +73,8 @@ namespace cli_menu {
       return wastedVec;
     }
 
-    template <typename T> std::vector<T> cleanDuplicateToMember(
+    template <typename T>
+    static std::vector<T> cleanDuplicateToMember(
       std::vector<T> *vec, T mem,
       // repeated and compared 'T' parameters
       std::function<bool(T,T)> equalRule = [](T rep, T com)->bool { return false; }
@@ -92,60 +102,15 @@ namespace cli_menu {
       return wastedVec;
     }
 
-    std::vector<LLUI> getDifferencesToSize(
+    static std::vector<LLUI> getDifferencesToSize(
       std::vector<LLUI> sizes,
       LLUI targetSize
-    ) {
-      LLUI max = targetSize;
+    );
 
-      for (auto &sz : sizes) {
-        if (targetSize > 0) {
-          if (sz > max && sz <= targetSize) max = sz;
-        }
-        else if (sz > max) max = sz;
-      }
-
-      std::vector<LLUI> differences;
-      for (auto &sz : sizes) { differences.push_back(max - sz); }
-      
-      return differences;
-    }
-
-    namespace { namespace SECRET {
-      void changeStringCase(std::string &str, bool isUpper) {
-        std::transform(
-          str.begin(),
-          str.end(),
-          str.begin(),
-          [&](unsigned char c) {
-            if (isUpper) return std::toupper(c);
-            else return std::tolower(c);
-          }
-        );
-      }
-
-      void changeStringsCase(VEC_STR *vecStr, bool isUpper) {
-        for (int i = 0; i < vecStr->size(); i++) {
-          changeStringCase(vecStr->at(i), isUpper);
-        }
-      }
-    }}
-
-    void stringToLowercase(std::string str) {
-      SECRET::changeStringCase(str, false);
-    }
-
-    void stringToUppercase(std::string str) {
-      SECRET::changeStringCase(str, true);
-    }
-
-    void stringsToLowercase(VEC_STR *vecStr) {
-      SECRET::changeStringsCase(vecStr, false);
-    }
-
-    void stringsToUppercase(VEC_STR *vecStr) {
-      SECRET::changeStringsCase(vecStr, true);
-    }
+    static void stringToLowercase(std::string str);
+    static void stringToUppercase(std::string str);
+    static void stringsToLowercase(VEC_STR *vecStr);
+    static void stringsToUppercase(VEC_STR *vecStr);
   };
 }
 
