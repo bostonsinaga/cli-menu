2c192fc8b9b66deae8a2e3391e6549c795dcf986
bostonsinaga <bostonsinga@gmail.com>
2024-06-05 22:04:59 +0700

    Declare 'testDashSign', 'isParameter', 'isToggle' methods
    Remove 'singleCommand'
    
    Fix constructors declaration

--- c++/executor.h
+++ c++/executor.h
@@ -1,20 +1,22 @@
 #ifndef __CLI_MENU__EXECUTOR_H__
 #define __CLI_MENU__EXECUTOR_H__
 
-#include <vector>
 #include "message.h"
 
 namespace cli_menu {
 
   class Executor {
   private:
-    Command *singleCommand = nullptr;
-    VEC_COM *commands = nullptr;
+    VEC_COM commands;
     std::string programName;
 
+    bool testDashSign(std::string &input, int dashCount);
+    bool isParameter(std::string &input);
+    bool isToggle(std::string &input);
+
   public:
-    Executor(std::string programName_in, Command *command_in);
-    Executor(std::string programName_in, std::vector<Command*> commands_in);
+    Executor(std::string programName_in, VEC_COM &commands_in);
+    Executor(std::string programName_in, const VEC_COM &commands_in);
     void iterate(int lineCount, char* lineArgs[]);
   };
 }
