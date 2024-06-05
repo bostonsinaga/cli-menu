81b6643f057fcc8af3a8d2ed3eea18084f8c74c6
bostonsinaga <bostonsinga@gmail.com>
2024-06-05 10:35:40 +0700

    The new static member variables are not a pointer
    Overloading constructor for both Parameter and Toggles
    
    Define a 'balance' method for both to apply 'getDifferencesToSize' (this for reusability)

--- c++/command.cpp
+++ c++/command.cpp
@@ -9,97 +9,151 @@ namespace cli_menu {
   // Parameters |
   //____________|
 
+  VEC_STR Parameters::names;
+  VEC_INT Parameters::types;
+  VEC_BOO Parameters::obligatories;
+  VEC_STR Parameters::descriptions;
+  VEC_STR Parameters::arguments;
+
   Parameters::Parameters(
-    VEC_STR names_in,
-    VEC_INT types_in,
-    VEC_BOO obligatories_in,
-    VEC_STR descriptions_in,
-    VEC_STR arguments_in
+    VEC_STR &names_in,
+    VEC_INT &types_in,
+    VEC_BOO &obligatories_in,
+    VEC_STR &descriptions_in
   ) {
-    names = &names_in;
-    types = &types_in;
-    obligatories = &obligatories_in;
-    descriptions = &descriptions_in;
-    arguments = &arguments_in;
-
-    std::vector<LLUI> differences = Util::getDifferencesToSize({
-      types->size(),
-      obligatories->size(),
-      descriptions->size(),
-      arguments->size()
-    }, names->size());
-
-    for (int i = 0; i < differences[0]; i++) { types->push_back(INT); }
-    for (int i = 0; i < differences[1]; i++) { obligatories->push_back(false); }
-    for (int i = 0; i < differences[2]; i++) { descriptions->push_back("No description."); }
-    for (int i = 0; i < differences[3]; i++) { arguments->push_back(""); }
+    names = names_in;
+    types = types_in;
+    obligatories = obligatories_in;
+    descriptions = descriptions_in;
+    balance();
   }
 
-  int Parameters::amount() { return names->size(); }
+  Parameters::Parameters(
+    const VEC_STR &names_in,
+    const VEC_INT &types_in,
+    const VEC_BOO &obligatories_in,
+    const VEC_STR &descriptions_in
+  ) {
+    names = names_in;
+    types = types_in;
+    obligatories = obligatories_in;
+    descriptions = descriptions_in;
+    balance();
+  }
+  
+  void Parameters::balance() {
+    std::vector<LLUI> differences = Util::getDifferencesToSize({
+      types.size(),
+      obligatories.size(),
+      descriptions.size(),
+      arguments.size()
+    }, names.size());
 
-  VEC_STR *Parameters::getNames() { return names; }
-  VEC_INT *Parameters::getTypes() { return types; }
-  VEC_BOO *Parameters::getObligatories() { return obligatories; }
-  VEC_STR *Parameters::getDescriptions() { return descriptions; }
-  VEC_STR *Parameters::getArguments() { return arguments; }
+    for (int i = 0; i < differences[0]; i++) { types.push_back(INT); }
+    for (int i = 0; i < differences[1]; i++) { obligatories.push_back(false); }
+    for (int i = 0; i < differences[2]; i++) { descriptions.push_back("No description."); }
+    for (int i = 0; i < differences[3]; i++) { arguments.push_back(""); }
+  }
+
+  void Parameters::setArguments(VEC_STR &arguments_in) {
+    arguments = arguments_in;
+    balance();
+  }
+
+  void Parameters::setArguments(const VEC_STR &arguments_in) {
+    arguments = arguments_in;
+    balance();
+  }
+
+  int Parameters::amount() { return names.size(); }
+
+  VEC_STR *Parameters::getNames() { return &names; }
+  VEC_INT *Parameters::getTypes() { return &types; }
+  VEC_BOO *Parameters::getObligatories() { return &obligatories; }
+  VEC_STR *Parameters::getDescriptions() { return &descriptions; }
+  VEC_STR *Parameters::getArguments() { return &arguments; }
 
   std::string Parameters::getName(int index) {
-    return Util::getIndexOfVector<std::string>(names, index, "");
+    return Util::getIndexOfVector<std::string>(&names, index, "");
   }
 
   int Parameters::getType(int index) {
-    return Util::getIndexOfVector<int>(types, index, 0);
+    return Util::getIndexOfVector<int>(&types, index, 0);
   }
 
   bool Parameters::getObligatory(int index) {
-    return Util::getIndexOfVector<bool>(obligatories, index, false);
+    return Util::getIndexOfVector<bool>(&obligatories, index, false);
   }
 
   std::string Parameters::getDescription(int index) {
-    return Util::getIndexOfVector<std::string>(descriptions, index, "");
+    return Util::getIndexOfVector<std::string>(&descriptions, index, "");
   }
 
   std::string Parameters::getArgument(int index) {
-    return Util::getIndexOfVector<std::string>(arguments, index, "");
+    return Util::getIndexOfVector<std::string>(&arguments, index, "");
   }
 
   //_________|
   // Toggles |
   //_________|
 
+  VEC_STR Toggles::names;
+  VEC_STR Toggles::descriptions;
+  VEC_BOO Toggles::states;
+
   Toggles::Toggles(
-    VEC_STR names_in,
-    VEC_STR descriptions_in,
-    VEC_BOO states_in
+    VEC_STR &names_in,
+    VEC_STR &descriptions_in
   ) {
-    names = &names_in;
-    descriptions = &descriptions_in;
-    states = &states_in;
-
-    std::vector<LLUI> differences = Util::getDifferencesToSize({
-      descriptions->size(), states->size()
-    }, names->size());
-
-    for (int i = 0; i < differences[0]; i++) { descriptions->push_back("No description."); }
-    for (int i = 0; i < differences[1]; i++) { states->push_back(false); }
+    names = names_in;
+    descriptions = descriptions_in;
+    balance();
   }
 
-  int Toggles::amount() { return names->size(); }
+  Toggles::Toggles(
+    const VEC_STR &names_in,
+    const VEC_STR &descriptions_in
+  ) {
+    names = names_in;
+    descriptions = descriptions_in;
+    balance();
+  }
 
-  VEC_STR *Toggles::getNames() { return names; }
-  VEC_STR *Toggles::getDescriptions() { return descriptions; }
-  VEC_BOO *Toggles::getStates() { return states; }
+  void Toggles::balance() {
+    std::vector<LLUI> differences = Util::getDifferencesToSize({
+      descriptions.size(), states.size()
+    }, names.size());
+
+    for (int i = 0; i < differences[0]; i++) { descriptions.push_back("No description."); }
+    for (int i = 0; i < differences[1]; i++) { states.push_back(false); }
+  }
+
+  void Toggles::setStates(VEC_BOO &states_in) {
+    states = states_in;
+    balance();
+  }
+
+  void Toggles::setStates(const VEC_BOO &states_in) {
+    states = states_in;
+    balance();
+  }
+
+  int Toggles::amount() { return names.size(); }
+
+  VEC_STR *Toggles::getNames() { return &names; }
+  VEC_STR *Toggles::getDescriptions() { return &descriptions; }
+  VEC_BOO *Toggles::getStates() { return &states; }
 
   std::string Toggles::getName(int index) {
-    return Util::getIndexOfVector<std::string>(names, index, "");
+    return Util::getIndexOfVector<std::string>(&names, index, "");
   }
 
   std::string Toggles::getDescription(int index) {
-    return Util::getIndexOfVector<std::string>(descriptions, index, "");
+    return Util::getIndexOfVector<std::string>(&descriptions, index, "");
   }
 
   bool Toggles::getState(int index) {
-    return Util::getIndexOfVector<bool>(states, index, "");
+    return Util::getIndexOfVector<bool>(&states, index, "");
   }
 
   //_________|
@@ -113,15 +167,15 @@ namespace cli_menu {
     Toggles *toggles_in,
     CLBK *callback_in
   ) {
-    name = &name_in;
-    description = &description_in;
+    name = name_in;
+    description = description_in;
     parameters = parameters_in;
     toggles = toggles_in;
     callback = callback_in;
   }
 
-  std::string Command::getName() { return *name; }
-  std::string Command::getDescription() { return *description; }
+  std::string Command::getName() { return name; }
+  std::string Command::getDescription() { return description; }
 
   void Command::execute() {
     (*callback)(
