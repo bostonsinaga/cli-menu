31fe6829bc5219d4d88b3bc7d30b9b565386f147
bostonsinaga <bostonsinga@gmail.com>
2024-07-16 18:16:02 +0700

    Test message

--- c++/main.cpp
+++ c++/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
-// #include "cli-menu.h"
+#include "cli-menu.h"
 
-int main(int argc, char *argv[0]) {
+int main(int argc, char *argv[]) {
+
+  cli_menu::Message::printError("please don't waste a time!");
 
   return 0;
 }
