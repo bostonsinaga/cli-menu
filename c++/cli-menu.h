6a74ff8413f20ca4e53ad79433bac60b920bfc13
bostonsinaga <bostonsinga@gmail.com>
2024-07-31 16:54:40 +0700

    Moved to sub folder

--- c++/cli-menu.h
+++
@@ -1,9 +0,0 @@
-#ifndef __CLI_MENU_H__
-#define __CLI_MENU_H__
-
-#include "util.h"
-#include "command.h"
-#include "message.h"
-#include "executor.h"
-
-#endif // __CLI_MENU_H__
--- c++/compile.bat
+++
@@ -1,2 +0,0 @@
-@REM g++ util.cpp command.cpp message.cpp executor.cpp main.cpp -o climenu
-g++ main.cpp -o climenu
