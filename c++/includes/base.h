fd6739ed95d7a01185e051a40d76fa2d030f09f4
bostonsinaga <bostonsinga@gmail.com>
2024-08-30 08:12:39 +0700

    Add 'PLAIN_CALLBACK' tydef

--- c++/includes/base.h
+++ c++/includes/base.h
@@ -11,8 +11,9 @@ namespace cli_menu {
 
   // callback format
   typedef std::function<void(mt::CR_VEC_STR, mt::CR_VEC2_DBL, mt::CR_VEC_BOL)> CALLBACK;
+  typedef std::function<void()> PLAIN_CALLBACK;
   typedef std::shared_ptr<CALLBACK> SP_CALLBACK;
-  typedef std::shared_ptr<void()> SP_PLAIN_CALLBACK;
+  typedef std::shared_ptr<PLAIN_CALLBACK> SP_PLAIN_CALLBACK;
   typedef const SP_CALLBACK& CR_SP_CALLBACK;
   typedef const SP_PLAIN_CALLBACK& CR_SP_PLAIN_CALLBACK;
 
