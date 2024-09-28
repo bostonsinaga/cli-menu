332c69e6679cfe13b84229107fe340012a54d6e8
bostonsinaga <bostonsinga@gmail.com>
2024-09-28 23:42:47 +0700

    Fix 'addItem' method. Call 'disguise' with boolean 'true' argument

--- c++/includes/command.cpp
+++ c++/includes/command.cpp
@@ -197,7 +197,7 @@ namespace cli_menu {
     if (command && !isSupporter()) {
 
       if (command->getInheritanceFlag() == PROGRAM) {
-        command->disguise();
+        command->disguise(true);
 
         Message::print(
           Message::STATUS::WARNING,
