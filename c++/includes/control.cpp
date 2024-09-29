2a246dc1c09f4a011ab06a735cf2823048e01d69
bostonsinaga <bostonsinga@gmail.com>
2024-09-29 18:07:53 +0700

    Set 2 version of options (.abbreviations and :acronyms)
    Add '.select or :s' handler
    
    Implement flags enum

--- c++/includes/control.cpp
+++ c++/includes/control.cpp
@@ -4,30 +4,40 @@
 #include "command.h"
 
 namespace cli_menu {
+
   const std::string
-    Command::Control::CANCEL = ":c",
-    Command::Control::ENTER = ":e",
-    Command::Control::NEXT = ":n";
+    Command::Control::CANCEL[2] = { ":c", ".cancel" },
+    Command::Control::ENTER[2] = { ":e", ".enter" },
+    Command::Control::NEXT[2] = { ":n", ".next" },
+    Command::Control::SELECT[2] = { ":s", ".select" };
 
   int Command::Control::test(std::string &str) {
     mt_uti::StrTools::changeStringToLowercase(str);
 
-    if (str == Command::Control::CANCEL) return 1;
-    else if (str == Command::Control::ENTER) return 2;
-    else if (str == Command::Control::NEXT) return 3;
+    for (int i = 0; i < 2; i++) {
+      if (str == Command::Control::CANCEL[i]) return CANCEL_FG;
+      else if (str == Command::Control::ENTER[i]) return ENTER_FG;
+      else if (str == Command::Control::NEXT[i]) return NEXT_FG;
+      else if (str == Command::Control::SELECT[i]) return SELECT_FG;
+    }
+
     return 0;
   }
 
   bool Command::Control::cancelTest(std::string &str) {
-    return test(str) == 1;
+    return test(str) == CANCEL_FG;
   }
 
   bool Command::Control::enterTest(std::string &str) {
-    return test(str) == 2;
+    return test(str) == ENTER_FG;
   }
 
   bool Command::Control::nextTest(std::string &str) {
-    return test(str) == 3;
+    return test(str) == NEXT_FG;
+  }
+
+  bool Command::Control::selectTest(std::string &str) {
+    return test(str) == SELECT_FG;
   }
 }
 
