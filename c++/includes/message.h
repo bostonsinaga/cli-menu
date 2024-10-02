49a7af6c9cc1378a5c914118a0dd7402b8f05fd6
bostonsinaga <bostonsinga@gmail.com>
2024-10-02 13:31:36 +0700

    Move and improve 'Color' class in separated file
    Change 'customColored' to 'Color::getString'

--- c++/includes/message.h
+++ c++/includes/message.h
@@ -1,22 +1,14 @@
 #ifndef __CLI_MENU__MESSAGE_H__
 #define __CLI_MENU__MESSAGE_H__
 
-#include "base.h"
+#include "color.h"
 
 namespace cli_menu {
 
   class Message {
-  private:
-    static constexpr mt::USI colorCodes[8] = {30, 31, 32, 33, 34, 35, 36, 37};
-    static constexpr mt::USI colorsCount = 8;
-
   public:
     enum STATUS {
       HINT, WARNING, ERROR, SUCCEED, CANCELED
-    };
-
-    enum COLOR {
-      BLACK, RED, GREEN, YELLOW, BLUE, PURPLE, LIGHT_BLUE, WHITE
     };
 
     static std::string getColoredTag(mt::CR_INT flag);
@@ -27,11 +19,6 @@ namespace cli_menu {
       std::string text,
       std::string name = "",
       bool toUppercase = true
-    );
-
-    static std::string customColored(
-      mt::USI code,
-      mt::CR_STR text
     );
 
     static void printBoundaryLine(
