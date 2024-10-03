48c38f01ff83dde6073be68668db332f30e25ad5
bostonsinaga <bostonsinga@gmail.com>
2024-10-03 14:50:48 +0700

    Unite individual names into 2D array
    Fix 'test' method. Acception to additional whitespaces
    
    Rearrange methods position

--- c++/includes/control.cpp
+++ c++/includes/control.cpp
@@ -5,46 +5,71 @@
 
 namespace cli_menu {
 
-  const std::string
-    Command::Control::CANCEL[2] = { ":c", ".cancel" },
-    Command::Control::ENTER[2] = { ":e", ".enter" },
-    Command::Control::NEXT[2] = { ":n", ".next" },
-    Command::Control::SELECT[2] = { ":s", ".select" };
+  const std::string Command::Control::NAMES[6][2] = {
+    { ":c", ".cancel" },
+    { ":s", ".select" },
+    { ":p", ".previous" },
+    { ":n", ".next" },
+    { ":b", ".back" },
+    { ":e", ".enter" }
+  };
 
-  int Command::Control::test(std::string &str) {
+  int Command::Control::test(std::string str) {
+
+    int spaceChange = 0;
     mt_uti::StrTools::changeStringToLowercase(str);
 
+    for (char &ch : str) {
+      if (ch != ' ' && ch != '\t') {
+        if (spaceChange == 0) spaceChange = 1;
+        else if (spaceChange == 2) return 0;
+      }
+      else if (
+        (ch == ' ' || ch == '\t') &&
+        spaceChange == 1
+      ) { spaceChange = 2; }
+    }
+
     for (int i = 0; i < 2; i++) {
-      if (str == Command::Control::CANCEL[i]) return CANCEL_FG;
-      else if (str == Command::Control::ENTER[i]) return ENTER_FG;
-      else if (str == Command::Control::NEXT[i]) return NEXT_FG;
-      else if (str == Command::Control::SELECT[i]) return SELECT_FG;
+      for (int j = 0; j < 6; j++) {
+        if (str.find(NAMES[j][i]) != std::string::npos) {
+          return j + 1;
+        }
+      }
     }
 
     return 0;
   }
 
   bool Command::Control::cancelTest(std::string &str) {
-    return test(str) == CANCEL_FG;
-  }
-
-  bool Command::Control::enterTest(std::string &str) {
-    return test(str) == ENTER_FG;
-  }
-
-  bool Command::Control::nextTest(std::string &str) {
-    return test(str) == NEXT_FG;
+    return test(str) == CANCEL + 1;
   }
 
   bool Command::Control::selectTest(std::string &str) {
-    return test(str) == SELECT_FG;
+    return test(str) == SELECT + 1;
+  }
+
+  bool Command::Control::previousTest(std::string &str) {
+    return test(str) == PREVIOUS + 1;
+  }
+
+  bool Command::Control::nextTest(std::string &str) {
+    return test(str) == NEXT + 1;
+  }
+
+  bool Command::Control::backTest(std::string &str) {
+    return test(str) == BACK + 1;
+  }
+
+  bool Command::Control::enterTest(std::string &str) {
+    return test(str) == ENTER + 1;
   }
 
   void Command::Control::printHelp() {
     std::cout << Color::getItalicString(
-      std::string("  .cancel = :c, .select   = :s,\n") +
-      std::string("  .next   = :n, .previous = :p,\n") +
-      std::string("  .back   = :b, .enter    = :e\n")
+      std::string("  .cancel   = :c, .select = :s,\n") +
+      std::string("  .previous = :p, .next   = :n,\n") +
+      std::string("  .back     = :b, .enter  = :e \n")
     );
   }
 }
