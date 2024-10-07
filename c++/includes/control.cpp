b013b22d5d233b59ba2fcde00194fb4ab60d372f
bostonsinaga <bostonsinga@gmail.com>
2024-10-07 08:15:56 +0700

    Fix 'test' method. Force return when 'abc123 space abc123' is detected. And then, find a match with pattern ' abc123 \t'
    Add '.help' and '.list' to 'NAMES'

--- c++/includes/control.cpp
+++ c++/includes/control.cpp
@@ -5,13 +5,15 @@
 
 namespace cli_menu {
 
-  const std::string Control::NAMES[6][2] = {
-    { ":c", ".cancel" },
-    { ":s", ".select" },
-    { ":p", ".previous" },
-    { ":n", ".next" },
-    { ":b", ".back" },
-    { ":e", ".enter" }
+  const std::string Control::NAMES[count][2] = {
+    { ".back", ":b" },
+    { ".cancel", ":c" },
+    { ".enter", ":e" },
+    { ".help", ":h" }, 
+    { ".list", ":l" },
+    { ".next", ":n" },
+    { ".previous", ":p" },
+    { ".select", ":s" }
   };
 
   int Control::test(std::string str) {
@@ -19,21 +21,56 @@ namespace cli_menu {
     int spaceChange = 0;
     mt_uti::StrTools::changeStringToLowercase(str);
 
+    static std::function<bool(mt::CR_CH)>
+      isSpace = [](mt::CR_CH ch)->bool {
+        return ch == ' ' || ch == '\t';
+      },
+      isNotSpace = [](mt::CR_CH ch)->bool {
+        return ch != ' ' && ch != '\t';
+      };
+
+    // force return when 'abc123 space abc123' is detected
     for (char &ch : str) {
-      if (ch != ' ' && ch != '\t') {
+      if (isNotSpace(ch)) {
         if (spaceChange == 0) spaceChange = 1;
         else if (spaceChange == 2) return 0;
       }
-      else if (
-        (ch == ' ' || ch == '\t') &&
-        spaceChange == 1
-      ) { spaceChange = 2; }
+      else if (spaceChange == 1 && isSpace(ch)) {
+        spaceChange = 2;
+      }
     }
 
+    // find a match with pattern ' abc123 \t'
     for (int i = 0; i < 2; i++) {
-      for (int j = 0; j < 6; j++) {
-        if (str.find(NAMES[j][i]) != std::string::npos) {
-          return j + 1;
+      for (int j = 0; j < count; j++) {
+
+        if (str.length() >= NAMES[j][i].length()) {
+          bool found = false;
+
+          for (int k = 0;
+            k <= str.length() - NAMES[j][i].length();
+            k++
+          ) {
+            bool kWillBreak = false;
+
+            for (int l = 0; l < NAMES[j][i].length(); l++) {
+              if (str[l+k] != NAMES[j][i][l]) {
+                if (isNotSpace(str[l+k])) {
+                  kWillBreak = true;
+                  found = false;
+                }
+                else if (found) {
+                  kWillBreak = true;
+                }
+                break;
+              }
+              else found = true;
+            }
+
+            if (kWillBreak) break;
+          }
+
+          if (found) return j+1;
         }
       }
     }
