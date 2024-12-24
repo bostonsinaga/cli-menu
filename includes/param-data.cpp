dcc8a5f484e06844b74dfd874490df218ef0ab8e
bostonsinaga <bostonsinga@gmail.com>
2024-12-24 23:02:09 +0700

    Move 'ParamData' struct from 'base.h' as member variables
    Define 'printList' outside. Now it is the only static member

--- includes/param-data.cpp
+++ includes/param-data.cpp
@@ -1,54 +1,67 @@
-#ifndef __CLI_MENU__DEBUG_H__
-#define __CLI_MENU__DEBUG_H__
+#ifndef __CLI_MENU__PARAM_DATA_H__
+#define __CLI_MENU__PARAM_DATA_H__
 
 #include "message.h"
 
 namespace cli_menu {
 
-  class Debug {
+  class ParamData;
+  typedef const ParamData& CR_PD;
+
+  class ParamData {
   private:
     template <typename T>
     static void printList(
       mt::CR_STR title,
-      mt::VEC2<T> vec2d
-    ) {
-      std::cout << title;
-
-      for (int i = 0; i < vec2d.size(); i++) {
-        std::cout << "\n  [";
-
-        for (int j = 0; j < vec2d[i].size(); j++) {
-
-          if (j > 0) std::cout << "   ";
-          std::cout << vec2d[i][j];
-
-          if (vec2d[i].size() > 1) {
-            if (j < vec2d[i].size() - 1) {
-              std::cout << ",\n";
-            }
-          }
-        }
-
-        std::cout << ']' << (i < vec2d.size() - 1 ? ',' : '\0');
-      }
-
-      std::cout << std::endl;
-    }
+      mt::CR_VEC2<T> vec2d
+    );
 
   public:
-    static void printParamDataArray(mt::CR_STR name, CLI_MENU_FUNPAR) {
+    mt::VEC2_STR texts;
+    mt::VEC2_LD numbers;
+    mt::VEC2_BOL conditions;
+
+    void printVector(mt::CR_STR name) {
 
       Message::printBoundaryLine(1, 1);
-      std::cout << "'" << name << "' ParamData (" << TEXTS.size() << "):";
+      std::cout << "'" << name << "' ParamData (" << ParamData::texts.size() << "):";
       Message::printBoundaryLine(1, 0);
 
-      Debug::printList<std::string>("\nTEXTS: ", TEXTS);
-      Debug::printList<mt::LD>("\nNUMBERS: ", NUMBERS);
-      Debug::printList<bool>("\nCONDITIONS: ", CONDITIONS);
+      ParamData::printList<std::string>("\nTEXTS: ", texts);
+      ParamData::printList<mt::LD>("\nNUMBERS: ", numbers);
+      ParamData::printList<bool>("\nCONDITIONS: ", conditions);
 
       Message::printBoundaryLine(0, 1);
     }
   };
+
+  template <typename T>
+  void ParamData::printList(
+    mt::CR_STR title,
+    mt::CR_VEC2<T> vec2d
+  ) {
+    std::cout << title;
+
+    for (int i = 0; i < vec2d.size(); i++) {
+      std::cout << "\n  [";
+
+      for (int j = 0; j < vec2d[i].size(); j++) {
+
+        if (j > 0) std::cout << "   ";
+        std::cout << vec2d[i][j];
+
+        if (vec2d[i].size() > 1) {
+          if (j < vec2d[i].size() - 1) {
+            std::cout << ",\n";
+          }
+        }
+      }
+
+      std::cout << ']' << (i < vec2d.size() - 1 ? ',' : '\0');
+    }
+
+    std::cout << std::endl;
+  }
 }
 
-#endif // __CLI_MENU__DEBUG_H__
+#endif // __CLI_MENU__PARAM_DATA_H__
