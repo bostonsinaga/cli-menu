a52b043a907a78636fc37844bf8d2f0f61a3802a
bostonsinaga <bostonsinga@gmail.com>
2024-12-25 12:33:36 +0700

    Move functions definitions from deleted 'base.h'
    Set class 'DashTest' class to 'dash_test' namespace

--- includes/dash-test.h
+++ includes/dash-test.h
@@ -1,18 +1,39 @@
-#ifndef __CLI_MENU__BASE_H__
-#define __CLI_MENU__BASE_H__
+#ifndef __CLI_MENU__DASH_TEST_H__
+#define __CLI_MENU__DASH_TEST_H__
 
 #include <memory>
 #include "mini-tools.h"
 
 namespace cli_menu {
+namespace dash_test {
 
-  class DashTest {
-  public:
-    static bool isSingle(mt::CR_STR str);
-    static bool isDouble(mt::CR_STR str);
-    static std::string cleanSingle(mt::CR_STR str);
-    static std::string cleanDouble(mt::CR_STR str);
-  };
-}
+  bool isSingle(mt::CR_STR str) {
+    if (str.length() > 1 && str[0] == '-') {
+      return true;
+    }
+    return false;
+  }
 
-#endif // __CLI_MENU__BASE_H__
+  bool isDouble(mt::CR_STR str) {
+    if (str.length() > 2 &&
+      str[0] == '-' && str[1] == '-'
+    ) { return true; }
+    return false;
+  }
+
+  std::string cleanSingle(mt::CR_STR str) {
+    if (isSingle(str)) {
+      return str.substr(1);
+    }
+    return "";
+  }
+
+  std::string cleanDouble(mt::CR_STR str) {
+    if (isDouble(str)) {
+      return str.substr(2);
+    }
+    return "";
+  }
+}}
+
+#endif // __CLI_MENU__DASH_TEST_H__
