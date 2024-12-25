170f740917c2beff3b21fc5e534b60c38a1f2f63
bostonsinaga <bostonsinga@gmail.com>
2024-12-25 18:42:44 +0700

    Move 'printList' definitions back to inside
    Reduce '..CALLBACK' typedef just 2
    
    Move inheritance flags from 'base.h'

--- includes/param-data.h
+++ includes/param-data.h
@@ -6,16 +6,8 @@
 
 namespace cli_menu {
 
-  class ParamData;
-  typedef const ParamData& CR_PD;
-
-  // callback formats
-  typedef std::function<void(CR_PD)> CALLBACK;
-  typedef std::function<void()> PLAIN_CALLBACK;
-  typedef std::shared_ptr<CALLBACK> SP_CALLBACK;
-  typedef std::shared_ptr<PLAIN_CALLBACK> SP_PLAIN_CALLBACK;
-  typedef const SP_CALLBACK& CR_SP_CALLBACK;
-  typedef const SP_PLAIN_CALLBACK& CR_SP_PLAIN_CALLBACK;
+  // inheritance flags
+  enum { COMMAND, PROGRAM, PARAMETER, TOGGLE };
 
   class ParamData {
   private:
@@ -23,7 +15,29 @@ namespace cli_menu {
     static void printList(
       mt::CR_STR title,
       mt::CR_VEC2<T> vec2d
-    );
+    ) {
+      std::cout << title;
+
+      for (int i = 0; i < vec2d.size(); i++) {
+        std::cout << "\n  [";
+
+        for (int j = 0; j < vec2d[i].size(); j++) {
+
+          if (j > 0) std::cout << "   ";
+          std::cout << vec2d[i][j];
+
+          if (vec2d[i].size() > 1) {
+            if (j < vec2d[i].size() - 1) {
+              std::cout << ",\n";
+            }
+          }
+        }
+
+        std::cout << ']' << (i < vec2d.size() - 1 ? ',' : '\0');
+      }
+
+      std::cout << std::endl;
+    }
 
   public:
     mt::VEC2_STR texts;
@@ -44,33 +58,9 @@ namespace cli_menu {
     }
   };
 
-  template <typename T>
-  void ParamData::printList(
-    mt::CR_STR title,
-    mt::CR_VEC2<T> vec2d
-  ) {
-    std::cout << title;
-
-    for (int i = 0; i < vec2d.size(); i++) {
-      std::cout << "\n  [";
-
-      for (int j = 0; j < vec2d[i].size(); j++) {
-
-        if (j > 0) std::cout << "   ";
-        std::cout << vec2d[i][j];
-
-        if (vec2d[i].size() > 1) {
-          if (j < vec2d[i].size() - 1) {
-            std::cout << ",\n";
-          }
-        }
-      }
-
-      std::cout << ']' << (i < vec2d.size() - 1 ? ',' : '\0');
-    }
-
-    std::cout << std::endl;
-  }
+  // callback formats
+  typedef std::function<void(ParamData&)> CALLBACK;
+  typedef std::function<void()> PLAIN_CALLBACK;
 }
 
 #endif // __CLI_MENU__PARAM_DATA_H__
