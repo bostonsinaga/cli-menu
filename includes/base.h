32582a7726169be308c2fa6ff3481f218e994718
bostonsinaga <bostonsinga@gmail.com>
2024-12-25 12:28:41 +0700

    Move 'ParamData' and callback formats to other files

--- includes/base.h
+++ includes/base.h
@@ -6,30 +6,6 @@
 
 namespace cli_menu {
 
-  // inheritance flags
-  enum { COMMAND, PROGRAM, PARAMETER, TOGGLE };
-
-  // callback format
-  typedef std::function<void(mt::CR_VEC_STR, mt::CR_VEC2_DBL, mt::CR_VEC_BOL)> CALLBACK;
-  typedef std::function<void()> PLAIN_CALLBACK;
-  typedef std::shared_ptr<CALLBACK> SP_CALLBACK;
-  typedef std::shared_ptr<PLAIN_CALLBACK> SP_PLAIN_CALLBACK;
-  typedef const SP_CALLBACK& CR_SP_CALLBACK;
-  typedef const SP_PLAIN_CALLBACK& CR_SP_PLAIN_CALLBACK;
-
-  struct ParamData {
-    mt::VEC_STR texts;
-    mt::VEC2_DBL numbers;
-    mt::VEC_BOL conditions;
-  };
-
-  template <typename T>
-  void SET_NO_NEGATIVE(T &num) {
-    if constexpr (mt::CheckType::isNumber<T>()) {
-      if (num < 0) num = 0;
-    }
-  }
-
   class DashTest {
   public:
     static bool isSingle(mt::CR_STR str);
