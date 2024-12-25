ef529cb7fecb675caccb22de947c0adb93f51216
bostonsinaga <bostonsinga@gmail.com>
2024-12-25 18:28:43 +0700

    Remove tutorial comment and callback preprocessors
    Include 'dash-test.h' and 'param-data.h'. Remove 'base.h'

--- includes/cli-menu.h
+++ includes/cli-menu.h
@@ -1,89 +1,11 @@
 #ifndef __CLI_MENU_H__
 #define __CLI_MENU_H__
 
-/**
- * These 3 parameters have the same size.
- * The index represents the input sequence.
- * Only one of the 3 parameters in one index has a meaningful value.
- *
- * Example:
- *   command = "set-spaces"
- *
- *   filename_flag = 0
- *   STR[filename_flag] == "foo.txt"   --> argument
- *   DBL[filename_flag] == {0}
- *   BOL[filename_flag] == false
- *
- *   count_flag = 1
- *   STR[count_flag] == ""
- *   DBL[count_flag] == {4}     --> argument
- *   BOL[count_flag] == false
- *
- *   copy_flag = 2
- *   STR[copy_flag] == ""
- *   DBL[copy_flag] == {0}
- *   BOL[copy_flag] == true   --> argument
- */
-
-#ifndef CM_FUNPAR
-#define CM_FUNPAR mt::CR_VEC_STR TEXTS, mt::CR_VEC2_DBL NUMBERS, mt::CR_VEC_BOL CONDITIONS
-#endif
-
-#ifdef CLI_MENU_FUNPAR
-#undef CLI_MENU_FUNPAR
-#endif
-
-#ifndef CLI_MENU_FUNPAR
-#define CLI_MENU_FUNPAR mt::CR_VEC_STR TEXTS, mt::CR_VEC2_DBL NUMBERS, mt::CR_VEC_BOL CONDITIONS
-#endif
-
-#ifndef CM_FUNCTION
-#define CM_FUNCTION(NAME) void NAME(CLI_MENU_FUNPAR)
-#endif
-
-#ifdef CLI_MENU_FUNCTION
-#undef CLI_MENU_FUNCTION
-#endif
-
-#ifndef CLI_MENU_FUNCTION
-#define CLI_MENU_FUNCTION(NAME) void NAME(CLI_MENU_FUNPAR)
-#endif
-
-/** CALLBACK */
-
-#ifndef CM_CALLBACK
-#define CM_CALLBACK(FUNCTION) std::make_shared<cli_menu::CALLBACK>(FUNCTION)
-#endif
-
-#ifdef CLI_MENU_CALLBACK
-#undef CLI_MENU_CALLBACK
-#endif
-
-#ifndef CLI_MENU_CALLBACK
-#define CLI_MENU_CALLBACK(FUNCTION) std::make_shared<cli_menu::CALLBACK>(FUNCTION)
-#endif
-
-/** PLAIN CALLBACK */
-
-#ifndef CM_PLAIN_CALLBACK
-#define CM_PLAIN_CALLBACK(FUNCTION) std::make_shared<cli_menu::PLAIN_CALLBACK>(FUNCTION)
-#endif
-
-#ifdef CLI_MENU_PLAIN_CALLBACK
-#undef CLI_MENU_PLAIN_CALLBACK
-#endif
-
-#ifndef CLI_MENU_PLAIN_CALLBACK
-#define CLI_MENU_PLAIN_CALLBACK(FUNCTION) std::make_shared<cli_menu::PLAIN_CALLBACK>(FUNCTION)
-#endif
-
-/** INCLUDES */
-
-#include "base.h"
+#include "dash-test.h"
 #include "color.h"
 #include "message.h"
-#include "debug.h"
 #include "control.h"
+#include "param-data.h"
 #include "command.h"
 #include "parameter.h"
 #include "toggle.h"
