d0329db4c31ff23e489df14d35ee2a6a13c4e931
bostonsinaga <bostonsinga@gmail.com>
2024-12-25 18:29:11 +0700

    Move methods definitions from 'base.cpp'

--- includes/dash-test.h
+++ includes/dash-test.h
@@ -4,35 +4,37 @@
 #include "mini-tools.h"
 
 namespace cli_menu {
-namespace dash_test {
 
-  bool isSingle(mt::CR_STR str) {
-    if (str.length() > 1 && str[0] == '-') {
-      return true;
+  class DashTest {
+  public:
+    static bool isSingle(mt::CR_STR str) {
+      if (str.length() > 1 && str[0] == '-') {
+        return true;
+      }
+      return false;
     }
-    return false;
-  }
 
-  bool isDouble(mt::CR_STR str) {
-    if (str.length() > 2 &&
-      str[0] == '-' && str[1] == '-'
-    ) { return true; }
-    return false;
-  }
-
-  std::string cleanSingle(mt::CR_STR str) {
-    if (isSingle(str)) {
-      return str.substr(1);
+    static bool isDouble(mt::CR_STR str) {
+      if (str.length() > 2 &&
+        str[0] == '-' && str[1] == '-'
+      ) { return true; }
+      return false;
     }
-    return "";
-  }
 
-  std::string cleanDouble(mt::CR_STR str) {
-    if (isDouble(str)) {
-      return str.substr(2);
+    static std::string cleanSingle(mt::CR_STR str) {
+      if (isSingle(str)) {
+        return str.substr(1);
+      }
+      return "";
     }
-    return "";
-  }
-}}
+
+    static std::string cleanDouble(mt::CR_STR str) {
+      if (isDouble(str)) {
+        return str.substr(2);
+      }
+      return "";
+    }
+  };
+}
 
 #endif // __CLI_MENU__DASH_TEST_H__
