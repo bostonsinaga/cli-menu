667af7a16cb07e41e31f7a2473bd1b07e4fc3aca
bostonsinaga <bostonsinga@gmail.com>
2025-01-07 14:29:00 +0700

    Moved from 'test'. This is the past trial

---
+++ includes/clipboard.h
@@ -0,0 +1,14 @@
+#ifndef __CLI_MENU__CLIPBOARD_H__
+#define __CLI_MENU__CLIPBOARD_H__
+
+#include "message.h"
+
+namespace cli_menu {
+
+  class Clipboard {
+  public:
+
+  };
+}
+
+#endif // __CLI_MENU__CLIPBOARD_H__
