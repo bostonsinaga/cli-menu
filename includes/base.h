0147844fe9f31045afd069c4762b9f2a734081a4
bostonsinaga <bostonsinga@gmail.com>
2025-02-01 08:10:49 +0700

    Move defininitions to source

--- includes/base.h
+++ includes/base.h
@@ -10,48 +10,13 @@ namespace cli_menu {
 
   class DashTest {
   private:
-    static bool isDigitLetter(mt::CR_STR str, mt::CR_INT index) {
-
-      if (mt_uti::StrTools::isDigit(str[index]) ||
-        mt_uti::StrTools::isLetter(str[index])
-      ) { return true; }
-
-      return false;
-    }
+    static bool isDigitLetter(mt::CR_STR str, mt::CR_INT index);
 
   public:
-    static bool isSingle(mt::CR_STR str) {
-
-      if (str.length() > 1 && str[0] == '-' &&
-        isDigitLetter(str, 1)
-      ) { return true; }
-
-      return false;
-    }
-
-    static bool isDouble(mt::CR_STR str) {
-
-      if (str.length() > 2 &&
-        str[0] == '-' && str[1] == '-' &&
-        isDigitLetter(str, 2)
-      ) { return true; }
-
-      return false;
-    }
-
-    static std::string cleanSingle(mt::CR_STR str) {
-      if (isSingle(str)) {
-        return str.substr(1);
-      }
-      return "";
-    }
-
-    static std::string cleanDouble(mt::CR_STR str) {
-      if (isDouble(str)) {
-        return str.substr(2);
-      }
-      return "";
-    }
+    static bool isSingle(mt::CR_STR str);
+    static bool isDouble(mt::CR_STR str);
+    static std::string cleanSingle(mt::CR_STR str);
+    static std::string cleanDouble(mt::CR_STR str);
   };
 }
 
