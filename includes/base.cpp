de4e9c815d5c5c14c08ebf4092d17ef63c983e92
bostonsinaga <bostonsinga@gmail.com>
2025-02-12 16:55:06 +0700

    Add 'Util' class that contains 'booleanTest' and 'revealBoolean' that moved from 'Toggle' class
    Remove 'windows.h' include

--- includes/base.cpp
+++ includes/base.cpp
@@ -5,6 +5,29 @@
 
 namespace cli_menu {
 
+  bool Util::isWhitespace(mt::CR_CH ch) {
+    return ch == ' ' || ch == '\t' || ch == '\n';
+  }
+
+  int Util::booleanTest {
+    if (str == "y" || str == "yes" ||
+      str == "1" || str == "true"
+    ) {
+      return 2;
+    }
+    else if (str == "n" || str == "no" ||
+      str == "0" || str == "false"
+    ) {
+      return 1;
+    }
+    return 0;
+  }
+
+  bool Util::revealBoolean(mt::CR_INT testedFlag) {
+    if (testedFlag > 1) return true;
+    return false;
+  }
+
   bool DashTest::isLetter(mt::CR_STR str, mt::CR_INT index) {
     if (mt_uti::StrTools::isLetter(str[index])) {
       return true;
--- includes/base.h
+++ includes/base.h
@@ -1,13 +1,23 @@
 #ifndef __CLI_MENU__BASE_H__
 #define __CLI_MENU__BASE_H__
 
-#include <windows.h>
 #include "mini-tools.h"
 
 namespace cli_menu {
 
   // inheritance flags
   enum { COMMAND, PROGRAM, PARAMETER, TOGGLE };
+
+  class Util {
+  public:
+    static bool isWhitespace(mt::CR_CH ch);
+
+    // returns 0 (other), 1 (false), 2 (true)
+    static int booleanTest(mt::CR_STR str);
+
+    // use 'booleanTest' first before using this
+    static bool revealBoolean(mt::CR_INT testedFlag);
+  };
 
   class DashTest {
   private:
