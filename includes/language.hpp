f5035b3bb9df763016ed7477d7eb1f164e64afa5
bostonsinaga <bostonsinga@gmail.com>
2025-07-01 16:25:26 +0700

    Remake with simplicity
    Using 'std::unordered_map' for 'messages'

--- includes/language.hpp
+++ includes/language.hpp
@@ -1,98 +1,53 @@
 #ifndef __CLI_MENU__LANGUAGE_HPP__
 #define __CLI_MENU__LANGUAGE_HPP__
 
-#include "base.hpp"
+#include "console.hpp"
 
 namespace cli_menu {
 
-  enum LANG_ENUM {
-    LANGERR_DIALOG_ALREADY_SELECTING,
-    LANGERR_DIALOG_BACK_AT_ROOT,
-    LANGERR_DIALOG_EMPTY_REQUIRED_ARGUMENTS,
-    LANGERR_DIALOG_FORBIDDEN_HIDDEN_TEXT_PASTING,
-    LANGERR_DIALOG_INCOMPLETE_ULTIMATE_PROCESS,
-    LANGERR_DIALOG_NO_ITEMS,
-    LANGERR_DIALOG_NO_NEIGHBORS,
-    LANGERR_DIALOG_NOT_FOUND,
-    LANGERR_DIALOG_UNPROCESSED_DIRECT_INPUTS,
-    LANGERR_MATCH_EMPTY_REQUIRED_ARGUMENTS,
-    LANGERR_MATCH_NEEDS_DIALOG_FOR_MANY,
-    LANGERR_MATCH_NEEDS_DIALOG_FOR_ONE,
-    LANGERR_MATCH_UNKNOWN_NEIGHBOR
-  };
-
-  typedef const LANG_ENUM& CR_LANGNUM;
-
   class Language {
+  public:
+    enum CODE {
+      ALREADY_SELECTING,
+      ARGUMENT_REQUIRED,
+      CLIPBOARD_OPEN_FAILURE,
+      CLIPBOARD_GET_FAILURE,
+      CLIPBOARD_LOCK_FAILURE,
+      CLIPBOARD_PASTED,
+      COMMAND_NOT_FOUND,
+      FORBIDDEN_HIDDEN_PASTE,
+      MIDDLE_DIALOG,
+      PARAMETER_ALONE,
+      PARAMETER_AT_LEAF,
+      PARAMETER_AT_ROOT,
+      PROGRAM_CANCELED,
+      PROGRAM_FAILED,
+      PROGRAM_SUCCEEDED
+    };
+
   private:
-    static const int maxSentencesSize = 13;
-    typedef mt::ARR<std::string, maxSentencesSize> LANGARR;
+    // special
+    inline static mt_util::Booleanizer booleanizer;
 
-    static const std::string defaultSentences[maxSentencesSize];
-    static mt::VEC<LANGARR> sentences;
-    static mt::VEC<std::string> collections;
-    static int currentCollectionIndex;
+    static constexpr int totalMessages = 15;
+    inline static std::sring currentISOCode = "en";
 
-    static std::string textCopy,
-      levelPlaceholder, namePlaceholder, taskPlaceholder;
-
-    static bool hasIndex(mt::CR_INT collectionIndex);
+    static mt::STRUNORMAP<mt::ARR_STR<totalMessages> messages;
+    static consoleCodes[totalMessages];
 
   public:
-    static int getCurrentCollectionIndex() {
-      return currentCollectionIndex;
-    }
+    Language() = delete;
 
-    static std::string getTextCopy() {
-      return textCopy;
-    }
-
-    // call this before using 'solveTemplate'
-    static void initTextCopy(CR_LANGNUM sentencesIndex);
-
-    // first called for 'sentences' initialization
-    static void reset();
-
-    static void addCollection(mt::CR_STR name);
-    static void switchCollection(mt::CR_INT collectionIndex);
-    static void removeCollection(mt::CR_INT collectionIndex);
-
-    static void translate(
-      mt::CR_INT collectionIndex,
-      CR_LANGNUM sentencesIndex,
-      mt::CR_STR sentence
+    static void registerCollection(
+      mt::CR_STR ISOCode,
+      mt::CR_ARR_STR<totalMessages> collection
     );
 
-    /**
-     * Call the 'mt_uti::StrTools::tidyUp'
-     * after this for neater text.
-     */
-    static void solveTemplate(
-      mt::CR_STR placeholder,
-      mt::CR_VEC_STR replacements,
-      mt::CR_PAIR<char> wordBracket = {'\0','\0'}
-    );
+    static void printResponse(const CODE &code);
+    static void changeISOCode(mt::CR_STR ISOCode);
 
-    static void solvePlaceholders(
-      CR_LANGNUM sentencesIndex,
-      mt::CR_VEC_STR levelReplacements = {},
-      mt::CR_VEC_STR nameReplacements = {},
-      mt::CR_VEC_STR typeReplacements = {}
-    );
-
-    /** Below are case sensitive */
-
-    static void setLevelPlaceholder(mt::CR_STR value) {
-      levelPlaceholder = value;
-    }
-
-    static void setNamePlaceholder(mt::CR_STR value) {
-      namePlaceholder = value;
-    }
-
-    static void setTaskPlaceholder(mt::CR_STR value) {
-      taskPlaceholder = value;
-    }
+    // special
+    static bool booleanize(CR_STR input);
   };
 }
 
--- src/language.cpp
+++ src/language.cpp
@@ -6,157 +6,90 @@
 namespace cli_menu {
 
   /** English Presets */
-  const std::string Language::defaultSentences[maxSentencesSize] = {
-
-    // LANGERR_DIALOG_ALREADY_SELECTING,
-    "already in selection mode",
-    // LANGERR_DIALOG_BACK_AT_ROOT
-    "no more groups above",
-    // LANGERR_DIALOG_EMPTY_REQUIRED_ARGUMENTS
-    "unable to ${TASK} without explicit arguments",
-    // LANGERR_DIALOG_FORBIDDEN_HIDDEN_TEXT_PASTING
-    "hidden text pasting is only available on insertion",
-    // LANGERR_DIALOG_INCOMPLETE_ULTIMATE_PROCESS
-    "unable to proceed until all required inputs are provided",
-    // LANGERR_DIALOG_NO_ITEMS
-    "this ${LEVEL} does not have any items",
-    // LANGERR_DIALOG_NO_NEIGHBORS
-    "this ${LEVEL} has no neighbors",
-    // LANGERR_DIALOG_NOT_FOUND
-    "${LEVEL} not found",
-    // LANGERR_DIALOG_UNPROCESSED_DIRECT_INPUTS
-    "unable to ${TASK} until the rest of direct inputs are processed",
-
-    // LANGERR_MATCH_EMPTY_REQUIRED_ARGUMENTS
-    "the ${LEVEL} named ${NAME} requires explicit arguments",
-    // LANGERR_MATCH_NEEDS_DIALOG_FOR_MANY
-    "the ${NAME} ${LEVEL} has ${LEVEL} that need to be used",
-    // LANGERR_MATCH_NEEDS_DIALOG_FOR_ONE
-    "the ${NAME} ${LEVEL} has ${LEVEL} named ${NAME} that need to be used",
-    // LANGERR_MATCH_UNKNOWN_NEIGHBOR
-    "unknown ${LEVEL} named ${NAME}"
+  std::string Language::messages["en"] = {
+    // ALREADY_SELECTING
+    "Already in selection mode.",
+    // ARGUMENT_REQUIRED
+    "Unable to process without explicit arguments."
+    // CLIPBOARD_OPEN_FAILURE,
+    "Failed to open clipboard.",
+    // CLIPBOARD_GET_FAILURE,
+    "Failed to get clipboard data.",
+    // CLIPBOARD_LOCK_FAILURE
+    "Failed to lock clipboard data.",
+    // CLIPBOARD_PASTED
+    "Pasted from clipboard.",
+    // COMMAND_NOT_FOUND
+    "Parameter not found.",
+    // FORBIDDEN_HIDDEN_PASTE
+    "Hidden text pasting is only available on insertion.",
+    // MIDDLE_DIALOG
+    "Unable to process until the rest of direct inputs are processed.",
+    // PARAMETER_ALONE
+    "This parameter has no neighbors.",
+    // PARAMETER_AT_LEAF
+    "This does not contain any parameters.",
+    // PARAMETER_AT_ROOT
+    "No more groups above.",
+    // PROGRAM_CANCELED
+    "Program canceled.",
+    // PROGRAM_FAILED
+    "Program failed.",
+    // PROGRAM_SUCCEEDED
+    "Program succeeded.",
   };
 
-  mt::VEC<Language::LANGARR> Language::sentences = {};
-  mt::VEC<std::string> Language::collections = {};
-  int Language::currentCollectionIndex = 0;
+  Console::CODE consoleCodes[totalMessages] = {
+    // ALREADY_SELECTING
+    Console::WARNING,
+    // ARGUMENT_REQUIRED
+    Console::ERROR,
+    // CLIPBOARD_OPEN_FAILURE,
+    Console::ERROR,
+    // CLIPBOARD_GET_FAILURE,
+    Console::ERROR,
+    // CLIPBOARD_LOCK_FAILURE
+    Console::ERROR,
+    // CLIPBOARD_PASTED
+    Console::HINT,
+    // COMMAND_NOT_FOUND
+    Console::ERROR,
+    // FORBIDDEN_HIDDEN_PASTE
+    Console::WARNING,
+    // MIDDLE_DIALOG
+    Console::ERROR,
+    // PARAMETER_ALONE
+    Console::WARNING,
+    // PARAMETER_AT_LEAF
+    Console::WARNING,
+    // PARAMETER_AT_ROOT
+    Console::WARNING,
+    // PROGRAM_CANCELED
+    Console::CANCEL,
+    // PROGRAM_FAILED
+    Console::ERROR,
+    // PROGRAM_SUCCEEDED
+    Console::CORRECT
+  };
 
-  std::string Language::textCopy = "",
-    Language::levelPlaceholder = "${LEVEL}",
-    Language::namePlaceholder = "${NAME}",
-    Language::taskPlaceholder = "${TASK}";
+  void Language::registerCollection(
+    mt::CR_STR ISOCode,
+    mt::CR_ARR_STR<totalMessages> collection
+  );
 
-  bool Language::hasIndex(mt::CR_INT collectionIndex) {
-    return mt_uti::VecTools<std::string>::hasIndex(
-      collections, collectionIndex
-    ) != -1;
-  }
-
-  void Language::reset() {
-    sentences = {LANGARR{""}};
-
-    for (int i = 0; i < maxSentencesSize; i++) {
-      sentences[0][i] = defaultSentences[i];
-    }
-  }
-
-  void Language::addCollection(mt::CR_STR name) {
-    if (mt_uti::VecTools<std::string>::getIndex(
-      collections, name
-    ) != -1) {
-      collections.push_back(name);
-      sentences.push_back(LANGARR{""});
-    }
-  }
-
-  void Language::switchCollection(mt::CR_INT collectionIndex) {
-    if (hasIndex(collectionIndex)) {
-      currentCollectionIndex = collectionIndex;
-    } 
-  }
-
-  void Language::removeCollection(mt::CR_INT collectionIndex) {
-
-    mt_uti::VecTools<std::string>::cutSingle(
-      collections,
-      collectionIndex,
-      true
-    );
-
-    mt_uti::VecTools<LANGARR>::cutSingle(
-      sentences,
-      collectionIndex,
-      true
+  void Language::printResponse(const CODE &code) {
+    Console::logResponse(
+      consoleCodes[code],
+      messages[code]
     );
   }
 
-  void Language::translate(
-    mt::CR_INT collectionIndex,
-    CR_LANGNUM sentencesIndex,
-    mt::CR_STR sentence
-  ) {
-    if (hasIndex(collectionIndex)) {
-      sentences[collectionIndex][sentencesIndex] = sentence;
-    }
+  void Language::changeISOCode(mt::CR_STR ISOCode) {
+    currentISOCode = ISOCode;
   }
 
-  void Language::initTextCopy(CR_LANGNUM sentencesIndex) {
-    textCopy = sentences[currentCollectionIndex][sentencesIndex];
-  }
-
-  void Language::solveTemplate(
-    mt::CR_STR placeholder,
-    mt::CR_VEC_STR replacements,
-    mt::CR_PAIR<char> wordBracket
-  ) {
-    size_t foundIndex;
-
-    for (mt::CR_STR word : replacements) {
-      foundIndex = textCopy.find(placeholder);
-
-      if (foundIndex != std::string::npos) {
-
-        // insert 'word' before 'placeholder'
-        textCopy.insert(
-          foundIndex,
-          wordBracket.first + word + wordBracket.second
-        );
-
-        // index moved right after the 'word'
-        foundIndex += (
-          bool(wordBracket.first)
-          + word.length()
-          + bool(wordBracket.second)
-        );
-
-        // remove 'placeholder' from 'textCopy'
-        textCopy = textCopy.substr(0, foundIndex)
-          + textCopy.substr(foundIndex + placeholder.length());
-      }
-    }
-  }
-
-  void Language::solvePlaceholders(
-    CR_LANGNUM sentencesIndex,
-    mt::CR_VEC_STR levelReplacements,
-    mt::CR_VEC_STR nameReplacements,
-    mt::CR_VEC_STR taskReplacements
-  ) {
-    initTextCopy(sentencesIndex);
-
-    solveTemplate(
-      levelPlaceholder, levelReplacements
-    );
-
-    solveTemplate(
-      namePlaceholder,
-      nameReplacements,
-      {'\'','\''}
-    );
-
-    solveTemplate(
-      taskPlaceholder, taskReplacements
-    );
+  bool Language::booleanize(CR_STR input) {
+    return booleanizer.test(input, currentISOCode);
   }
 }
 
