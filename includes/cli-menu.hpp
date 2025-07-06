40c48b8700f399d1ab55465d98456bf727f0373d
bostonsinaga <bostonsinga@gmail.com>
2025-07-06 18:42:01 +0700

    Include new and reorder

--- includes/cli-menu.hpp
+++ includes/cli-menu.hpp
@@ -1,16 +1,16 @@
 #ifndef __CLI_MENU_HPP__
 #define __CLI_MENU_HPP__
 
-#include "base.hpp"
 #include "color.hpp"
-#include "message.hpp"
-#include "clipboard.hpp"
 #include "control.hpp"
-#include "result-inputs.hpp"
+#include "result.hpp"
+#include "console.hpp"
+#include "language.hpp"
+#include "clipboard.hpp"
 #include "command.hpp"
-#include "parameter.hpp"
+#include "word.hpp"
+#include "number.hpp"
 #include "toggle.hpp"
-#include "program.hpp"
 
 namespace cm = cli_menu;
 
