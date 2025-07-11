4caf28477ec1a3fcf6fef8ed401024090b38f1df
bostonsinaga <bostonsinga@gmail.com>
2025-07-11 17:56:26 +0700

    Add more description to tell about hidden text copy & paste

--- includes/clipboard.hpp
+++ includes/clipboard.hpp
@@ -1,18 +1,25 @@
 #ifndef __CLI_MENU__CLIPBOARD_HPP__
 #define __CLI_MENU__CLIPBOARD_HPP__
 
-#include "language.hpp"
+#include "control.hpp"
 
 namespace cli_menu {
 
-  /** ONLY FOR STRING COPY! */
+  /**
+   * COPY & PASTE (only available for string).
+   * The main purpose for this is to enable pasting large
+   * amounts of hidden text in the CLI via the interface,
+   * using the ':c' symbol from 'Control' within
+   * the 'Command::dialog' method by using
+   * 'Command::copyPaste' method.
+   */
   class Clipboard {
   public:
+    Clipboard() = delete;
+
     static std::string pasteText();
     static mt::VEC_LD pasteNumbers();
     static mt::VEC_BOL pasteConditions();
-
-    Clipboard() = delete;
   };
 }
 
