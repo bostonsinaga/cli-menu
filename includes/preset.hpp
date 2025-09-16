40034f31bac6453a80270e05729bf6eac72f2653
bostonsinaga <bostonsinga@gmail.com>
2025-09-16 10:34:07 +0700

    Improve 'Preset::applyFileIn' to expands wildcard. Improve 'Preset::applyFileOut' to write to file with fallback filename selection
    Add 'Preset::completeFileInWildcards' and 'Preset::setFileOut'
    
    Remove 'Preset::applyFileFull'

--- includes/preset.hpp
+++ includes/preset.hpp
@@ -6,29 +6,63 @@
 namespace cli_menu {
 
   class Preset final {
+  private:
+    // get multiple filenames from wildcard pattern
+    static void completeFileInWildcards(Command *node);
+
+    /**
+     * Write 'Result::outputs' to file.
+     * Part of 'applyFileOut' preset callback.
+     */
+    static bool setFileOut(
+      Command *node,
+      std::string &filename
+    );
+
   public:
     /** Read/write text file with custom handler */
 
     static void applyFileIn(
       Creator *owner,
-      CR_COMMAND_CALLBACK customCallback
+      mt::CR_BOL isRequired,
+      COMMAND_CALLBACK customCallback
     );
 
     static void applyFileOut(
       Creator *owner,
-      CR_COMMAND_CALLBACK customCallback
+      mt::CR_BOL isRequired,
+      COMMAND_CALLBACK customCallback
     );
 
-    /** Read/write text file with default handler */
+    /**
+     * Text file reader preset (can read multiple files).
+     * Accept wildcards to get multiple files.
+     * 
+     * If 'isRequired' is false and filename is not provided,
+     * the callback will return true without displaying error message.
+     */
+    static void applyFileIn(
+      Creator *owner,
+      mt::CR_BOL isRequired
+    );
 
-    // can read multiple files
-    static void applyFileIn(Creator *owner);
-
-    // only write to the last filename
-    static void applyFileOut(Creator *owner);
-
-    // call the 2 methods above at once
-    static void applyFile(Creator *owner);
+    /**
+     * Text file writer preset.
+     * 
+     * Only write to the last filename of 'PRESET_OUT' argument.
+     * If it not provided ('isRequired' is false), the last argument of 'PRESET_IN' will be used.
+     * If still not provided, the program keyword will be used as the filename
+     * with 'fileOutDefaultExtension' as its extension.
+     * 
+     * If 'isOptional' is true and filename is not provided,
+     * the callback will return true without creating file
+     * and without displaying error message.
+     */
+    static void applyFileOut(
+      Creator *owner,
+      mt::CR_BOL isRequired,
+      mt::CR_BOL isOptional
+    );
 
     // these are owned by all commands by default
     static void applyHelp(Creator *owner);
--- src/preset.cpp
+++ src/preset.cpp
@@ -1,50 +1,101 @@
 #ifndef __CLI_MENU__PRESET_CPP__
 #define __CLI_MENU__PRESET_CPP__
 
+#include <windows.h>
 #include "preset.hpp"
 
 namespace cli_menu {
 
+  void Preset::completeFileInWildcards(Command *node) {
+
+    std::string pattern;
+    WIN32_FIND_DATAA findFileData;
+    HANDLE hFind;
+
+    std::string::size_type lastSlashIndex;
+    std::string basePath;
+
+    for (int i = 0; i < Result::numberOfWords(node); i++) {
+      pattern = Result::getWordAt(node, i);
+
+      if (pattern.find('*') != std::string::npos ||
+        pattern.find('?') != std::string::npos
+      ) {
+        hFind = FindFirstFileA(pattern.c_str(), &findFileData);
+
+        // keep the pattern path
+        lastSlashIndex = pattern.find_last_of("\\/");
+        basePath = (lastSlashIndex == std::string::npos) ?
+          "" : pattern.substr(0, lastSlashIndex + 1);
+
+        // expand wildcard pattern to path with filename
+        if (hFind != INVALID_HANDLE_VALUE) {
+          Result::popWord(node);
+          i--;
+
+          do {
+            if (!(findFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)) {
+              Result::pushWord(node, basePath + findFileData.cFileName);
+              i++;
+            }
+          } while (FindNextFileA(hFind, &findFileData) != 0);
+
+          FindClose(hFind);
+        }
+      }
+    }
+  }
+
   void Preset::applyFileIn(
     Creator *owner,
-    CR_COMMAND_CALLBACK customCallback
+    mt::CR_BOL isRequired,
+    COMMAND_CALLBACK customCallback
   ) {
     Word *in = owner->createWord(
       Langu::agePreset::getKeyword(PRESET_IN),
       Langu::agePreset::getDescription(PRESET_IN)
     );
 
-    in->makeRequired();
+    if (isRequired) in->makeRequired();
     owner->pushInputCallbacks(customCallback, in);
   }
 
   void Preset::applyFileOut(
     Creator *owner,
-    CR_COMMAND_CALLBACK customCallback
+    mt::CR_BOL isRequired,
+    COMMAND_CALLBACK customCallback
   ) {
     Word *out = owner->createWord(
       Langu::agePreset::getKeyword(PRESET_OUT),
       Langu::agePreset::getDescription(PRESET_OUT)
     );
 
+    if (isRequired) out->makeRequired();
     owner->pushOutputCallbacks(customCallback, out);
   }
 
-  void Preset::applyFileIn(Creator *owner) {
+  void Preset::applyFileIn(
+    Creator *owner,
+    mt::CR_BOL isRequired
+  ) {
     applyFileIn(
-      owner,
-      [&](Command *node)->bool {
+      owner, isRequired,
+      [](Command *node)->bool {
         bool found = false;
         std::string filename;
+        completeFileInWildcards(node);
 
+        // read multiple files
         for (int i = 0; i < Result::numberOfWords(node); i++) {
           filename = Result::getWordAt(node, i);
 
           if (mt_uti::Scanner::isFileExist(filename)) {
             found = true;
 
+            // read file content
             Result::pushOutput(
-              owner, mt_uti::Scanner::readFileString(filename)
+              static_cast<Command*>(node->getParent()),
+              mt_uti::Scanner::readFileString(filename)
             );
           }
           else Langu::ageMessage::printTemplateResponse(
@@ -53,45 +104,123 @@ namespace cli_menu {
           );
         }
 
-        return found;
+        return found || !Result::numberOfWords(node);
       }
     );
   }
 
-  void Preset::applyFileOut(Creator *owner) {
-    applyFileOut(
-      owner,
-      [&](Command *node)->bool {
-        std::string filename = Result::getLastWord(node);
+  bool Preset::setFileOut(
+    Command *node,
+    std::string &filename
+  ) {
+    if (!filename.empty()) {
 
+      std::string outputText = Result::concatOutputs(
+        static_cast<Command*>(node->getParent())
+      );
+
+      if (!outputText.empty()) {
+
+        // existing file require verification to be overwritten
         if (mt_uti::Scanner::isFileExist(filename) &&
-          !Toggle::instantBooleanize()
+          !Toggle::instantBooleanize(
+            SENTENCE_FILE_OVERWRITE_QUESTION,
+            filename
+          )
         ) {
           int counter = 0;
-          std::string numname;
+          std::string prefix = filename, suffix, numname;
 
+          // separate the name and extension from the filename
+          for (int i = filename.length() - 1; i >= 0; i--) {
+            if (filename[i] == '.') {
+              prefix = filename.substr(0, i);
+              suffix = filename.substr(i);
+              break;
+            }
+          }
+
+          // trying to make a unique name with numbers in brackets
           do {
-            numname = filename + '[' + std::to_string(counter) + ']';
+            numname = prefix + Langu::agePreset::fileOutBracketsForNumbering.first
+              + std::to_string(counter) + Langu::agePreset::fileOutBracketsForNumbering.second + suffix;
             counter++;
           } while (mt_uti::Scanner::isFileExist(numname));
 
           filename = numname;
         }
 
-        mt_uti::Printer::write(
-          Result::concatOutputs(owner),
-          filename,
-          false
-        );
+        // write string vector to text file
+        if (mt_uti::Printer::write(
+          outputText, filename, false
+        )) {
+          Langu::ageMessage::printTemplateResponse(
+            SENTENCE_FILE_WRITE_SUCCEED,
+            filename
+          );
+
+          return true;
+        }
+      }
+      else Langu::ageMessage::printTemplateResponse(
+        SENTENCE_EMPTY_OUTPUT,
+        static_cast<Command*>(node->getParent())->getKeyword()
+      );
+    }
+
+    return false;
+  }
+
+  void Preset::applyFileOut(
+    Creator *owner,
+    mt::CR_BOL isRequired,
+    mt::CR_BOL isOptional
+  ) {
+    applyFileOut(
+      owner, isRequired,
+      isOptional ?
+      [](Command *node)->bool {
+        std::string filename = Result::getLastWord(node);
+        setFileOut(node, filename);
+        return true;
+      } :
+      [](Command *node)->bool {
+        std::string filename = Result::getLastWord(node);
+
+        if (filename.empty()) {
+
+          // set filename with file-in last argument
+          node->iterate(
+            mt_ds::GeneralTree::RIGHT,
+            [&](mt_ds::LinkedList *linkedList)->bool {
+
+              if (static_cast<Command*>(linkedList)->getKeyword()
+                == Langu::agePreset::getKeyword(PRESET_IN)
+              ) {
+                filename = Result::getLastWord(static_cast<Command*>(linkedList));
+                return false;
+              }
+
+              return true;
+            }
+          );
+
+          // set filename with program keyword
+          if (filename.empty()) {
+            filename = static_cast<Command*>(node->getRoot())->getKeyword()
+              + Langu::agePreset::fileOutDefaultExtension;
+          }
+        }
+
+        // file write failed
+        if (!setFileOut(node, filename)) {
+          Langu::ageMessage::printResponse(SENTENCE_FILE_WRITE_FAILURE);
+          return false;
+        }
 
         return true;
       }
     );
-  }
-
-  void Preset::applyFile(Creator *owner) {
-    Preset::applyFileIn(owner);
-    Preset::applyFileOut(owner);
   }
 
   void Preset::applyHelp(Creator *owner) {
