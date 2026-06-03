073e3b89c519902e5a6774020643b976acd592d3
bostonsinaga <bostonsinga@gmail.com>
2026-06-03 09:05:13 +0700

    Move executable file to '../test/'

--- build-scripts/compile.cmd
+++ build-scripts/compile.cmd
@@ -2,7 +2,7 @@
 ninja -C ../build
 
 IF EXIST "..\build\test\users.exe" (
-  COPY "..\build\test\users.exe" "."
+  COPY "..\build\test\users.exe" "..\test\users.exe"
 ) ELSE (
   ECHO Executable does not exist!
 )
